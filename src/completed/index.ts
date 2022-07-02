56380191dfc0c83daa1ab7776d4aef289f4c00ee
jwy <jiangweiye@outlook.com>
2022-07-02 16:43:51 +0800

    feature : update pnpm to npm

--- src/completed/index.ts
+++ src/completed/index.ts
@@ -47,7 +47,6 @@ class CompletionItemProvider implements vscode.CompletionItemProvider {
 export function activate(context: vscode.ExtensionContext) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-
     const CompletedCodeConfig = vscode.workspace.getConfiguration('CompletedCode').get('config');
 
     // @ts-ignore
